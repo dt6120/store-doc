5237f7212e4cab098ef8c81fbca4293f54ae6d9a
Dhruv Takwal <dhruvtakwal6120@gmail.com>
2022-01-16 15:01:52 +0530

    Add tests for StoreDoc contract

---
+++ test/storeDoc.test.js
@@ -0,0 +1,89 @@
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
+
+describe("StoreDoc", () => {
+    let contract, owner, authUploader, user, isUploadingRestricted;
+
+    before(async () => {
+        [owner, authUploader, user] = await ethers.getSigners()
+    })
+
+    beforeEach(async () => {
+        const Contract = await ethers.getContractFactory("StoreDoc")
+        isUploadingRestricted = true;
+        contract = await Contract.deploy(authUploader.address, isUploadingRestricted)
+        await contract.deployed()
+    })
+
+    describe("deployment", () => {
+        it("owner set successfully", async () => {
+            expect(await contract.owner()).to.equal(owner.address)
+        })
+
+        it("auth uploader set successfully", async () => {
+            expect(await contract.authUploader()).to.equal(authUploader.address)
+        })
+
+        it("uploading restriction set successfully", async () => {
+            expect(await contract.isUploadingRestricted()).to.equal(isUploadingRestricted)
+        })
+    })
+
+    describe("uploadDoc", () => {
+        const mediaHash = "0x12345678901234567890"
+
+        describe("with empty hash", () => {
+            it("throws an error", async () => {
+                await expect(
+                    contract.connect(authUploader).uploadDoc("")
+                ).to.be.revertedWith("Document hash cannot be empty")
+            })
+        })
+
+        describe("with restricted uploading", () => {
+            describe("user is not auth uploader", () => {
+                it("throws an error", async () => {
+                    await expect(
+                        contract.connect(user).uploadDoc(mediaHash)
+                    ).to.be.revertedWith("Only authorized address can upload document")
+                })
+            })
+
+            describe("user is auth uploader", () => {
+                it("uploads document successfully", async () => {
+                    const tx = await contract.connect(authUploader).uploadDoc(mediaHash)
+                    const { events } = await tx.wait()
+                    const event = events.find(event => event.event === "DocUploaded")
+
+                    expect(event).to.not.be.undefined
+                })
+            })
+        })
+
+        describe("with unrestricted uploading", () => {
+            beforeEach(async () => {
+                await contract.connect(owner).setRestrictedUploading(false)
+            })
+
+            describe("user is auth uploader", () => {
+                it("uploads document successfully", async () => {
+                    const tx = await contract.connect(authUploader).uploadDoc(mediaHash)
+                    const { events } = await tx.wait()
+                    const event = events.find(event => event.event === "DocUploaded")
+
+                    expect(event).to.not.be.undefined
+                })
+            })
+
+            describe("user is not auth uploader", () => {
+                it("uploads document successfully", async () => {
+                    const tx = await contract.connect(user).uploadDoc(mediaHash)
+                    const { events } = await tx.wait()
+                    const event = events.find(event => event.event === "DocUploaded")
+
+                    expect(event).to.not.be.undefined
+                })
+            })
+        })
+    })
+})
